b2521d9a558de6adc1b42ed5b25abb25e49a1d96
AKASH <akash.arthanur26@gmail.com>
2022-12-20 21:32:16 +0530

    added table rows

--- src/components/tablearea/ContactTable/ContactTable.jsx
+++ src/components/tablearea/ContactTable/ContactTable.jsx
@@ -3,23 +3,25 @@ import "./ContactTable.css"
 export default function ContactTable() {
     return (
         <div id='contactTable-wrap'>
-            <table>
+            <table id='table-wrap'>
 
-                <tr>
-                    <th>Name</th>
-                    <th>Designation</th>
-                    <th>Industry</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Country</th>
-                    <th>Action</th>
+                <tr id='head-wrap'>
+                    <th ><input type="checkbox" name="" id="" /></th>
+                    <th >Name</th>
+                    <th >|Designation</th>
+                    <th >|Industry</th>
+                    <th >|Company</th>
+                    <th >|Email</th>
+                    <th>|Phone Number</th>
+                    <th>|Country</th>
+                    <th>|Action</th>
                 </tr>
 
 
                 <tr>
+                    <tr><input type="checkbox" name="" id="" /></tr>
                     <td>Akash</td>
-                    <td>CEO</td>
+                    <td >CEO</td>
                     <td>Nvidia</td>
                     <td>Software</td>
                     <td>akash.arthanur26@gmail.com</td>
